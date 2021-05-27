f554302f805b314240a809274cd36ef4f9a713ed
Na Than Assh Antti <na_th_an@hotmail.com>
2021-05-27 13:19:50 +0200

    And now for something completely different

---
+++ src_cpc/dev/isr.h
@@ -0,0 +1,31 @@
+// MTE MK1 v4.8
+// Copyleft 2010-2013, 2020-2021 by The Mojon Twins
+
+// ISR routine
+// isr
+
+#asm
+	defw 0	// 2 bytes libres
+#endasm
+
+void ISR (void) {	
+	#ifdef MIN_FAPS_PER_FRAME
+		#asm
+				ld  hl, _isrc
+				inc (hl)
+			#if defined MODE_128K_DUAL && defined ENABLE_ARKOS
+					ld  a, (_ay_player_on)
+					or  a
+					jr  z, skip_arkos
+					
+					ld  b, ARKOS_RAM
+					call SetRAMBank
+					call ARKOS_ADDRESS_ATPLAY
+					ld  b, 0
+					call SetRAMBank
+
+				.skip_arkos
+			#endif
+		#endasm
+	#endif
+}
