5d3d0cb2385adb1325798ad76e6ddbed58b20e2f
na_th_an <na_th_an@hotmail.com>
2022-03-13 14:18:44 +0100

    Vlido lideo Leed

--- games_cpc/ramiro3/dev/system/loadercpc.asm
+++ games_cpc/ramiro3/dev/system/loadercpc.asm
@@ -30,13 +30,13 @@ org $BA00
 
 ; Load binary
 
-	ld      ix, $BA00 - 18676
-	ld      de, 18676
+	ld      ix, $BA00 - 18720
+	ld      de, 18720
 	call    cpct_miniload_asm
 
 ; Depack binary
 
-	ld      hl, $BA00 - 18676
+	ld      hl, $BA00 - 18720
 	ld      de, $0400
 	call    dzx7_standard
 
